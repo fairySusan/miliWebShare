7b47e4415751d4eeec659fcd49a96f4b35c73575
taorui <taorui@mobmili.com>
2022-09-15 15:03:04 +0800

    update 倒计时按钮

---
+++ components/button/count-down-button.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-button :disabled="isClicked" size="large" type="primary" class="code-button" @click="$emit('click')">
+    {{ isClicked ? `${count}s`: '获取验证码'}}
+  </a-button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const $emit = defineEmits<{
+  (event: 'click'):void
+}>()
+
+const count = ref(60)
+const isClicked = ref(false)
+
+const startCountDown = () => {
+  isClicked.value = true
+  setInterval(() => {
+    if (count.value > 0) {
+      count.value--
+    } else {
+      count.value = 60
+      isClicked.value = false
+    }
+  }, 1000)
+}
+
+defineExpose({
+  startCountDown
+})
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,3 @@
 export { default as ResetButton} from './button/reset-button.vue';
 export { default as SearchButton} from './button/search-button.vue';
+export { default as CountDownButton } from './button/count-down-button.vue';
