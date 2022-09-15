5566dc92575104b2335fcb95ae634bde0dbea4a1
taorui <taorui@mobmili.com>
2022-09-15 14:12:46 +0800

    add: reset-button

---
+++ components/button/reset-button.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-button @click="$emit('click')">
+    <template #icon><icon-refresh /></template>
+  </a-button>
+</template>
+
+<script setup lang="ts">
+  defineEmits<{
+    (event: 'click', e: Event):void
+  }>()
+</script>
